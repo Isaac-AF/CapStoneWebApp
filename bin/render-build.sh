f1cb64ac1f6726d5e792b9560b7fc7f5496d9221
Isaac-AF <Isaac.Frank@ChicagoBooth.edu>
2025-05-26 04:21:06 +0000

    BRUH

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,13 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-
- # print out which ENV and database URL we’ve got
- echo ">>> RAILS_ENV is [${RAILS_ENV:-not set}]"
- echo ">>> DATABASE_URL is [$DATABASE_URL]"
-
- # run migrations in production with trace
- bundle exec rake db:migrate RAILS_ENV=production --trace
-
-
 bundle exec rake db:migrate
