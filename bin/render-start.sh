0d94c469eddf5b6542ff67bf8fce006c17f91f81
Isaac-AF <Isaac.Frank@ChicagoBooth.edu>
2025-05-27 01:25:36 +0000

    Still troubleshooting

--- bin/render-start.sh
+++ bin/render-start.sh
@@ -4,9 +4,13 @@
  # tell Rails we’re in production
  export RAILS_ENV=production
 
+  echo "=== DB URL: $DATABASE_URL"
+  bundle exec rails runner 'pp ActiveRecord::Base.connection_db_config.url'
+
+
  # run migrations + seed once (on every deploy/start)
- bundle exec rake db:migrate
- bundle exec rake db:seed
+ bundle exec rake db:migrate --trace
+ bundle exec rake db:seed --trace
 
  # now start the web server and bind to the port Render provides
  exec bundle exec puma -C config/puma.rb
