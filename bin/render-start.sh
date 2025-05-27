80a93f03b4e4d3267da85e64b90c64711ce8a2a2
Isaac-AF <Isaac.Frank@ChicagoBooth.edu>
2025-05-27 01:11:50 +0000

    Bro I just want to deploy my web app is that too much to ask

--- bin/render-start.sh
+++ bin/render-start.sh
@@ -1,6 +1,12 @@
  #!/usr/bin/env bash
  set -o errexit
 
+ # tell Rails we’re in production
  export RAILS_ENV=production
+
+ # run migrations + seed once (on every deploy/start)
  bundle exec rake db:migrate
  bundle exec rake db:seed
+
+ # now start the web server and bind to the port Render provides
+ exec bundle exec puma -C config/puma.rb
