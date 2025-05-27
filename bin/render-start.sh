e32cb408d56b5dfb9b609d6978de7250feba4451
Isaac-AF <Isaac.Frank@ChicagoBooth.edu>
2025-05-27 01:31:25 +0000

    Why is this such a struggle

--- bin/render-start.sh
+++ bin/render-start.sh
@@ -4,9 +4,14 @@
  # tell Rails we’re in production
  export RAILS_ENV=production
 
-  echo "=== DB URL: $DATABASE_URL"
-  bundle exec rails runner 'pp ActiveRecord::Base.connection_db_config.url'
+# DEBUG: print the DATABASE_URL
+echo "=== DB URL: $DATABASE_URL"
 
+# DEBUG: list existing tables
+bundle exec rails runner 'pp ActiveRecord::Base.connection.tables'
+
+# DEBUG: list applied migration versions
+bundle exec rails runner 'pp ActiveRecord::SchemaMigration.all.map(&:version)'
 
  # run migrations + seed once (on every deploy/start)
  bundle exec rake db:migrate --trace
